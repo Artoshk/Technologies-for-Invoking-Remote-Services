0636e83e2873d149d5ba5ede902d7a90d83b19fc
Anderson Karl <andersonlkarl@hotmail.com>
2023-06-04 21:14:45 -0300

    graphql ts removed

--- ts_grpc/src/entity/Usuario.ts
+++ ts_grpc/src/entity/Usuario.ts
@@ -1,22 +1,17 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { Playlist } from './Playlist';
-import { Field, ObjectType } from "type-graphql";
 
-@ObjectType()
 @Entity('Usuario', {synchronize: false})
 export class Usuario {
   @PrimaryGeneratedColumn({ type: 'int', name: 'ID' })
   ID!: number;
 
-  @Field((_type) => String)
   @Column({ type: 'varchar', length: 100, nullable: true })
   Nome: string | undefined;
 
-  @Field((_type) => Number)
   @Column({ type: 'int', nullable: true })
   Idade!: number | undefined;
 
-  @Field((_type) => [Playlist])
   @OneToMany(() => Playlist, playlist => playlist.usuario)
   playlists: Playlist[] | undefined;
 }
